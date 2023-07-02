d86370674cb72be48fc5285084d1d74fa24e2dda
Shubhanshi <jainshubhanshi1002@gmail.com>
2023-07-02 23:53:27 +0530

    contact section fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import contact from '../components/assets/conracts.gif'
 
 const Form = () => {
     const [formData, setFormData]= useState({
@@ -18,22 +19,26 @@ message: '',
     };
 
   return (
+    <div className='flex flex-col justify-between items-center py-20 md:flex-row md:px-8'>
+      <div className='flex justify-center items-center md:justify-between'>
+<img src={contact} alt='contact' className='w-3/4'/>
+      </div>
    <form onSubmit={handleSubmit}>
-    <label>Name: </label>
-    <input type='text' name='name' value={formData.name} onChange={handleChange} required/>
+    {/* <label>Name: </label> */}
+    <input type='text' name='name' value={formData.name} onChange={handleChange} placeholder='Name' required className='border my-2 rounded-md'/>
     
     <br>
     </br>
-    <label>Email:  </label>
-    <input name='email'  type='email' value={formData.email} onChange={handleChange} required/>
+    {/* <label>Email:  </label> */}
+    <input name='email'  type='email' value={formData.email} onChange={handleChange} placeholder='Email' required className='border my-2 rounded-md'/>
    <br></br>
-    <label>Message:</label>
-    <textarea type="text" name="messaage" value={formData.message} onChange={handleChange} required/>
+    {/* <label>Message: </label> */}
+    <textarea name="message"  type="textarea" value={formData.message} onChange={handleChange} placeholder='Message' required className='border my-2 rounded-md w-full'/>
    <br></br>
     <button type='submit'>submit</button>
     
    </form>
-   
+   </div>
   )
 }
 
